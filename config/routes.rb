f8328a52569f87b6f87977aff01b3f71a40d205a
Surya Law <suryadi.lawira@yahoo.com>
2017-05-08 20:20:40 +1000

    wip

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,16 @@
 Rails.application.routes.draw do
-  resources :users
+  get 'sessions/new'
+
   root 'static_pages#home'
   get 'contact-us', to: 'static_pages#contact'
   get 'about-us', to: 'static_pages#about'
   get 'helloworld', to: 'static_pages#helloworld'
+
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
+
+  resources :users
 
   resources :articles do
     resources :comments
